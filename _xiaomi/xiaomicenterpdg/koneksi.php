56493a9c56e47569008aecee800ab24344911192
kiting <fauzanahmad174@gmail.com>
2020-10-15 15:56:24 +0700

    perubahan isi database

--- _xiaomi/xiaomicenterpdg/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'localhost';
-$username = 'root'; 
-$password = ''; 
-$database = 'xiaomicenterpdg';
-
-$pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
-?>
